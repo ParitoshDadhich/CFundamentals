6ec7ba2ecd9a2a35a3aa2d4d5045eaca51fa14ea
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2023-10-08 21:58:57 +0530

    Update mergeTwoLL.c

--- LinkedList/mergeTwoLL.c
+++ LinkedList/mergeTwoLL.c
@@ -1,93 +1,101 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
 
-struct Node{
+typedef struct Node Node;
+
+struct Node {
     int data;
-    struct Node *next;
-} *first=NULL, *second=NULL, *third=NULL;
+    Node *next;
+} *first = NULL, *second = NULL, *third = NULL;
 
-extern bool flag = false;
 
-void create(struct Node *node, int arr[], int n){
-    node = (struct Node *)malloc(sizeof(struct Node));
-    node->data = arr[0];
-    node->next = NULL;
+void createList(int arr[], int n, Node **header);
+void displayList(Node *node);
+void merge(Node *first, Node *second);
+void displayList(Node *node);
 
-    // need to assign node to first
-    if(!flag){
-        first = node;
-        flag = true;
-    } else{
-        second = node;
-    }
-
-    struct Node *t, *last=node;
-    for(int i=1; i<n; i++){
-        t = (struct Node *)malloc(sizeof(struct Node));
-        t->data = arr[i];
-        t->next = NULL;
-
-        last->next = t;
-        last = t;
-    }
+int main() {
+    int arr1[] = {1,3,5,7};
+    int arr2[] = {2,4,6,8,9,10};
+    createList(arr1, 4, &first);
+    displayList(first);
+    createList(arr2, 6, &second);
+    displayList(second);
+    printf("first %d , second %d", first->data, second->data);
+    merge(first, second);
+    displayList(third);
 }
 
-void display(struct Node *node){
-    while(node != NULL){
-        printf("%d ", node->data);
+Node *createNode(int val) {
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->data = val;
+    node->next = NULL;
+    return node;
+}
+
+void createList(int arr[], int n, Node **header) {
+    *header = createNode(arr[0]);
+    
+    Node *temp = *header;
+    for(int i=1; i<n; i++){
+        Node *tempNode = createNode(arr[i]);
+        temp->next = tempNode;
+        temp = tempNode;
+    }
+    printf("List created!\n");
+}
+
+void merge(Node *first, Node *second) {
+    Node *temp = NULL;
+    
+    if(first->data > second->data) {
+        third = second;
+        temp = second;
+        second = second->next;
+        temp->next = NULL;
+    } else {
+        third = first;
+        temp = first;
+        first = first->next;
+        temp->next = NULL;
+    }
+    
+    while(first != NULL && second != NULL) {
+        if(first->data > second->data) {
+            temp->next = second;
+            temp = second;
+            second = second->next;
+            temp->next = NULL;
+        } else {
+            temp->next = first;
+            temp = first;
+            first = first->next;
+            temp->next = NULL;
+        }
+    }
+    
+    while(first != NULL) {
+        temp->next = first;
+        temp = first;
+        first = first->next;
+        temp->next = NULL;
+    }
+    
+     while(second != NULL) {
+        temp->next = second;
+        temp = second;
+        second = second->next;
+        temp->next = NULL;
+    }
+    
+    printf("Merging process completed!\n");
+}
+
+void displayList(Node *node) {
+    printf("Displaying linked list!\n");
+    while(node != NULL) {
+        printf("%d\n", node->data);
         node = node->next;
     }
-}
-
-void merge(struct Node *first, struct Node *second){
-    struct Node *last;
-    if(first->data < second->data){
-        third = last = first;
-        first = first->next;
-        // third->next = NULL;
-    }else{
-        third = last = second;
-        second = second->next;
-        // third->next = NULL;
-    }
-
-    while(first && second){
-        if(first->data < second->data){
-            last->next = first;
-            last = first;
-            first = first->next;
-            // last->next = NULL;
-        }
-        else{
-            last->next = second;
-            last = second;
-            second = second->next;
-            // last->next = NULL;
-        }
-    }
-
-    if(first)
-        last->next = first;
-    if(second)
-        last->next = second;
-
-    return;
-}
-
-int main(){
-    int arr1[] = {1,3,5};
-    int arr2[] = {2,4,6,8,10};
-
-    create(first, arr1, 3);
-    display(first);
-    printf("  ");
-    create(second, arr2, 5);
-    display(second);
-
-    printf("                 ");
-    merge(first, second);
-    display(third);
-
-    return 0;
+    printf("Displayed all elements!\n");
 }
